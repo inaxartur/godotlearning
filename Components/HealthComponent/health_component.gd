b3fe1dcc639979c08add2a71e08c6f7745325684
inaxartur <167907279+inaxartur@users.noreply.github.com>
2025-12-06 00:28:31 +0100

    Isometric
    - fixed dash
    - early components
    - changed files directories

---
+++ Components/HealthComponent/health_component.gd
@@ -0,0 +1,16 @@
+extends Node2D
+class_name HealthComponent
+
+@export var MAX_HEALTH := 100.0
+
+var health : float
+
+func _ready() -> void:
+	health = MAX_HEALTH
+
+
+func damage(attack: AttackComponent):
+	health -= attack.attack_damage
+	
+	if health < 0:
+		get_parent().queue_free()
